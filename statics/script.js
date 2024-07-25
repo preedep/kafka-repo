a94343dc3878af9ec3000d3b9ce38224e41d3222
Preedee Ponchevin <preedee.digital@gmail.com>
2024-07-25 07:19:57 +0700

    Add web static to be web front end

--- statics/script.js
+++ statics/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+function load_dropdown_owner_of_topics() {
     // API endpoint
     const apiEndpoint = '/api/v1/apps';
 
@@ -6,18 +6,45 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch(apiEndpoint)
         .then(response => response.json())
         .then(data => {
-            const dropdown = document.getElementById('dropdown');
-            // Loop through the data and create option elements
-            const items = data.data;
-            for (let i = 0; i < items.length; i++) {
-                const item = items[i];
-                const option = document.createElement('option');
-                option.value = item;
-                option.textContent = item;
-                dropdown.appendChild(option);
-            }
+            const dropdown = document.getElementById('dropdown-owner-topic');
+            bind_data_for_option(data, dropdown);
             console.log(data);
 
         })
         .catch(error => console.error('Error fetching data:', error));
+}
+// Loop through the data and create option elements
+function bind_data_for_option(data, dropdown) {
+    const items = data.data;
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        const option = document.createElement('option');
+        option.value = item;
+        option.textContent = item;
+        dropdown.appendChild(option);
+    }
+}
+
+function load_dropdown_app_consumer() {
+    // API endpoint
+    const apiEndpoint = '/api/v1/consumers';
+
+    // Fetch data from the API
+    fetch(apiEndpoint)
+        .then(response => response.json())
+        .then(data => {
+            const dropdown = document.getElementById('dropdown-consumer-app');
+            bind_data_for_option(data, dropdown);
+            console.log(data);
+
+        })
+        .catch(error => console.error('Error fetching data:', error));
+
+}
+
+// Load the dropdown when the DOM is ready
+
+document.addEventListener('DOMContentLoaded', function() {
+    load_dropdown_owner_of_topics();
+    load_dropdown_app_consumer();
 });
