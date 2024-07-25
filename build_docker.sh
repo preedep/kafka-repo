98d219e8115e99470bfd7d289a5f08c9e64d1fee
Preedee Ponchevin <preedee.digital@gmail.com>
2024-07-25 20:50:14 +0700

    Add web static to be web front end - add code for search

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,4 @@
 export DOCKER_BUILDKIT=1
-docker build -t kafka-repo-service:latest .
+docker build --platform linux/amd64 -t kafka-repo-service:latest .
+docker tag kafka-repo-service:latest nickmsft/kafka-repo-service:latest
+docker push nickmsft/kafka-repo-service:latest
