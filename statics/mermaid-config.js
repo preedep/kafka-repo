6847e6712549baf37db52acd263cc3edc55c178d
Preedee Ponchevin <preedee.digital@gmail.com>
2024-07-25 17:20:44 +0700

    Add web static to be web front end - add code for search

--- statics/mermaid-config.js
+++ statics/mermaid-config.js
@@ -4,27 +4,20 @@ export function initializeMermaid() {
     mermaid.initialize({ startOnLoad: false });
 }
 export function renderMermaid(diagramText) {
+     console.log("Rendering Mermaid diagram");
     const container = document.getElementById('mermaid-container');
-    let mermaidAPI = mermaid.mermaidAPI;
-
-
+    if (!container) {
+        console.error('Mermaid container not found');
+        return;
+    }
     container.innerHTML = ''; // Clear previous content
-/*
-    var insertSvg = function (svgCode, bindFunctions) {
-        container.innerHTML = svgCode;
-    };
-    mermaidAPI.render('mermaid-chart', diagramText, insertSvg)
-        .then(() => {
-            console.log(`Successfully rendered chart mermaid-svg.`);
-        })
-        .catch((e) => {
-            console.log(`FAILED to render chart mermaid-svg: ${e}`);
+
+    try {
+        mermaid.mermaidAPI.render('mermaid-diagram', diagramText, (svgCode) => {
+            container.innerHTML = svgCode;
+            console.log("Successfully rendered chart mermaid-svg.");
         });
- */
-
-
-
-    mermaidAPI.render('mermaid-diagram', diagramText, (svgCode) => {
-        container.innerHTML = svgCode;
-    }, container);
+    } catch (e) {
+        console.error(`FAILED to render chart mermaid-svg: ${e}`);
+    }
 }
